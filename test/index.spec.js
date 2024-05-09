69b449bb2f7e51b4d7f9adaf70d127bf34880a44
naftalimurgor <murgornaftali@gmail.com>
2024-05-09 10:14:46 +0300

    update testcases

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,49 +1,49 @@
 var should = require('chai').should();
-var sb = require('../index.js');
+var bglunits = require('../index.js');
 
-describe('#toBitcoin', function() {
-  it('converts simple integer amounts', function() {
-    sb.toBitcoin(100000000).should.equal(1);
-    sb.toBitcoin(123456789012345).should.equal(1234567.89012345);
+describe('#toBGL', function () {
+  it('converts simple integer amounts', function () {
+    bglunits.toBGL(100000000).should.equal(1);
+    bglunits.toBGL(123456789012345).should.equal(1234567.89012345);
   });
-  it('converts simple string amounts', function() {
-    sb.toBitcoin('100000000').should.equal(1);
-    sb.toBitcoin('123456789012345').should.equal(1234567.89012345);
-  });
-
-  it('converts to Bitcoin, not to Satoshi', function() {
-    sb.toBitcoin(98765).should.not.equal(9876500000000);
+  it('converts simple string amounts', function () {
+    bglunits.toBGL('100000000').should.equal(1);
+    bglunits.toBGL('123456789012345').should.equal(1234567.89012345);
   });
 
-  it('converts and handles corner case rounding', function() {
-    sb.toBitcoin(46).should.equal(.00000046);
+  it('converts to Bitcoin, not to Satoshi', function () {
+    bglunits.toBGL(98765).should.not.equal(9876500000000);
   });
 
-  it('handles TypeError input', function() {
-    sb.toBitcoin.bind(this, true).should.throw('toBitcoin must be called on a number or string');
-    sb.toBitcoin.bind(this, 1.1).should.throw('toBitcoin must be called on a whole number or string format whole number');
+  it('converts and handles corner case rounding', function () {
+    bglunits.toBGL(46).should.equal(.00000046);
+  });
+
+  it('handles TypeError input', function () {
+    bglunits.toBGL.bind(this, true).should.throw('toBGL must be called on a number or string');
+    bglunits.toBGL.bind(this, 1.1).should.throw('toBGL must be called on a whole number or string format whole number');
   });
 });
 
-describe('#toSatoshi', function() {
-  it('converts simple integer amounts', function() {
-    sb.toSatoshi(0.00000001).should.equal(1);
-    sb.toSatoshi(98765).should.equal(9876500000000);
+describe('#toSatoshiUnits', function () {
+  it('converts simple integer amounts', function () {
+    bglunits.toSatoshiUnits(0.00000001).should.equal(1);
+    bglunits.toSatoshiUnits(98765).should.equal(9876500000000);
   });
-  it('converts simple string amounts', function() {
-    sb.toSatoshi('0.00000001').should.equal(1);
-    sb.toSatoshi('98765').should.equal(9876500000000);
-  });
-
-  it('converts to Satoshi, not to Bitcoin', function() {
-    sb.toSatoshi(123456789012345).should.not.equal(1234567.89012345);
+  it('converts simple string amounts', function () {
+    bglunits.toSatoshiUnits('0.00000001').should.equal(1);
+    bglunits.toSatoshiUnits('98765').should.equal(9876500000000);
   });
 
-  it('converts and handles corner case rounding', function() {
-    sb.toSatoshi(4.6).should.equal(460000000);
+  it('converts to Satoshi, not to Bitcoin', function () {
+    bglunits.toSatoshiUnits(123456789012345).should.not.equal(1234567.89012345);
   });
 
-  it('handles TypeError input', function() {
-    sb.toSatoshi.bind(this, true).should.throw('toSatoshi must be called on a number or string');
+  it('converts and handles corner case rounding', function () {
+    bglunits.toSatoshiUnits(4.6).should.equal(460000000);
+  });
+
+  it('handles TypeError input', function () {
+    bglunits.toSatoshiUnits.bind(this, true).should.throw('toSatoshiUnits must be called on a number or string');
   });
 });
